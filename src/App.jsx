66ed5b4b28f37058db48a7100e975537bd817684
ARABBOY KAYUMOV <arabboykayumov@ARABBOYs-MacBook-Pro.local>
2025-01-13 02:22:08 -0800

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
-const App=() =>{
+const App = () => {
   return (
-    <div>
-      3D Develper Portfolio
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
+        <Navbar />
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
